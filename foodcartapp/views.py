8bd923d82320adee1d6bb165310365e8d5431e2d
Jana <yanix2x2@gmail.com>
2025-09-17 16:37:52 +0300

    Add data validation for register_order

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -3,19 +3,11 @@ from django.templatetags.static import static
 from django.forms.models import model_to_dict
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework import generics
-import json
-from pprint import pprint
+from rest_framework import status
+import phonenumbers
 
 
 from .models import Product, Order, OrderedProduct, Restaurant
-# from .serializers import OrderSerializer
-
-
-# class OrderAPIView(generics.ListCreateAPIView):
-#     queryset = Order.objects.all()
-#     serializer_class = OrderSerializer
 
 
 def banners_list_api(request):
@@ -73,33 +65,85 @@ def product_list_api(request):
 @api_view(['POST'])
 def register_order(request):
     print('У-ля-ля')
+
+    required_fields = ['products', 'firstname', 'lastname', 'phonenumber', 'address']
+    for field in required_fields:
+        if field not in request.data:
+            return Response({
+                'error': f'Обязательное поле отсутствует: {field}'
+            }, status=status.HTTP_400_BAD_REQUEST)
+
     try:
-        order_form = request.data
-
-        order = Order.objects.create(
-            firstname=order_form.get('firstname'),
-            lastname=order_form.get('lastname'),
-            phonenumber=order_form.get('phonenumber'),
-            address=order_form.get('address'),
-            restaurant=Restaurant.objects.get(id=1),
-        )
-
-        for product in order_form.get('products'):
-            OrderedProduct.objects.create(
-                product_id=product.get('product'),
-                quantity=product.get('quantity'),
-                order=order
-            )
-
-        return Response({'order done': model_to_dict(order, exclude=['phonenumber'])})
-
-    except ValueError:
-        return JsonResponse({
-            'error': 'Оу, май',
+        phone_number = request.data['phonenumber']
+        parsed_number = phonenumbers.parse(phone_number, 'RU')
+        if not phonenumbers.is_valid_number(parsed_number):
+            return Response({
+                'error': 'Неверный формат номера телефона'
+            }, status=status.HTTP_400_BAD_REQUEST)
+    except phonenumbers.NumberParseException:
+        return Response({
+            'error': 'Неверный формат номера телефона'
         })
 
+    products = request.data['products']
 
-@api_view(['POST'])
-def get_hello(request):
-    name = request.data.get('hi') or 'motherfucker'
-    return Response({'hello': name})
+    if products is None:
+        return Response({
+            'error': 'products: Это поле не может быть пустым.'
+        }, status=status.HTTP_400_BAD_REQUEST)
+
+    if not isinstance(products, list):
+        received_type = type(products).__name__
+        return Response({
+            'error': f'products: Ожидался list со значениями, но был получен "{received_type}".'
+        }, status=status.HTTP_400_BAD_REQUEST)
+    
+    if len(products) == 0:
+        return Response({
+            'error': 'products: Этот список не может быть пустым.'
+        }, status=status.HTTP_400_BAD_REQUEST)
+    
+    for index, product in enumerate(products):
+        if not isinstance(product, dict):
+            return Response({
+                'error': f'products[{index}]: Ожидался объект, но был получен {type(product).__name__}.'
+            }, status=status.HTTP_400_BAD_REQUEST)
+        
+        if 'product' not in product:
+            return Response({
+                'error': f'products[{index}]: Отсутствует обязательное поле "product".'
+            })
+        
+        if 'quantity' not in product:
+            return Response({
+                'error': f'products[{index}]: Отсутствует обязательное поле "quantity".'
+            }, status=status.HTTP_400_BAD_REQUEST)
+        
+        if not isinstance(product['quantity'], int) or product['quantity'] <= 0:
+            return Response({
+                'error': f'products[{index}]: quantity должно быть положительным целым числом.'
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+    order_form = request.data
+
+    order = Order.objects.create(
+        firstname=order_form.get('firstname'),
+        lastname=order_form.get('lastname'),
+        phonenumber=order_form.get('phonenumber'),
+        address=order_form.get('address'),
+        restaurant=Restaurant.objects.get(id=1),
+    )
+
+    for product in order_form.get('products'):
+        OrderedProduct.objects.create(
+            product_id=product.get('product'),
+            quantity=product.get('quantity'),
+            order=order
+        )
+
+    return Response({'order done': model_to_dict(order, exclude=['phonenumber'])})
+
+    # except ValueError:
+    #     return JsonResponse({
+    #         'error': 'Оу, май',
+    #     })
